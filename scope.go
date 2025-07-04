e19db3e0c7ec8a9b1b88155871ba4c84c281c5e8
Nasim <nasimcse16@gmail.com>
2025-07-04 14:45:50 +0600

    update

--- scope.go
+++ scope.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	"example.com/mathlib"
 )
 
 var a = 20
 var b = 30
 
-func add(x int, y int) int{
+func add(x int, y int) int {
 	return x + y
 }
 
@@ -18,4 +20,8 @@ func main() {
 	fmt.Println("p+q = ", add(p, q))
 	fmt.Println("a+b = ", add(a, b))
 	fmt.Println("a+p = ", add(a, p))
+
+	// package level scope
+	// go mod init example.com
+	fmt.Println(mathlib.Add(p, q))
 }
