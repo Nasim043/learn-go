aed6edbd84330080b7990138ee8aa107ce27fda7
Nasim043 <nasimcse16@gmail.com>
2025-07-08 16:25:15 +0600

    update

--- array.go
+++ array.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var arr [2] int
-	fmt.Println(len(arr))
+	var arr [2]int
+	fmt.Println(arr) // [0 0]
 
-	for i := 0; i < len(arr); i++{
+	arr[0] = 1
+	fmt.Println(arr) // [1 0]
+
+	for i := 0; i < len(arr); i++ {
 		fmt.Println(arr[i])
 	}
+
+	// declare and initialize
+	arr2 := [5]int{1, 2, 3}
+	fmt.Println(arr2) // [1 2 3 0 0]
 }
